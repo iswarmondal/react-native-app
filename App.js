bbe424eb7eb2d0707ac757ea4ef4d8009949cd56
Iswar <rick731202@gmail.com>
2022-05-07 07:53:28 +0530

    learning TextInput

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
 
 export default function App() {
+
+  const pressHandler = () =>{
+    setPreson({name : 'Violet', age: 20})
+  }
+
+  const [person, setPreson] = useState({name: 'Iswar', age: 19});
+  const [username, setUsername] = useState("Jhon Snow")
+  const [userage, setUserage] = useState('99')
+
   return (
     <View style={styles.container}>
       <View style={styles.navbar}>
         <Text>This is the navbar</Text>
       </View>
       <View style={styles.body}>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Text>Hello everyone 👋</Text>
+        <Text>I'm {person.name}, a {person.age} years old developer</Text>
+        <View style={styles.btnContainer}>
+          <Button title="The other self ?" onPress={pressHandler}/>
+        </View>
+        <Text>Your name is {username}, aged {userage} ?</Text>
+        <TextInput 
+        style={styles.textInput}
+        placeholder="e.g. Jhon Snow"
+        keyboardType='default'
+        onChangeText={(val)=>setUsername(val)}
+        />
+
+        <TextInput 
+        style={styles.textInput}
+        placeholder="e.g. 99"
+        keyboardType='numeric' 
+        onChangeText={val=>setUserage(val)}
+        />
       </View>
     </View>
   );
@@ -30,5 +57,16 @@ const styles = StyleSheet.create({
   body:{
     backgroundColor: 'yellow',
     padding: 50
+  },
+  btnContainer:{
+    padding: 30
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#777',
+    borderRadius: 7,
+    width: 200,
+    padding: 7,
+    marginVertical: 2
   }
 });
